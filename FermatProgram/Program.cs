4bb83dcf60e90b33ad304817906b999b64bd2b3e
Lusiuk <brcloud1@yandex.ru>
2025-12-09 01:01:43 +0300

    Worked out some serious code

---
+++ FermatProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace FermatProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 3, b = 4, c = 5, n = 3;
+            WriteLine($"Проверяет теорему Ферма для a={a}, b={b}, c={c}, n={n}");
+            
+            if (IsFermatSatisfied(a, b, c, n))
+            {
+                //Если a^n + b^n = c^n, то теорема нарушена (только для n > 2)
+                WriteLine("Теорема НАРУШЕНА!");
+            }
+            else
+            {
+                //Если a^n + b^n != c^n, то теорема не нарушена
+                WriteLine("Теорема НЕ НАРУШЕНА!");
+            }
+        }
+        
+        //Возвращает true, если a^n + b^n = c^n
+        public static bool IsFermatSatisfied(int a, int b, int c, int n) => Math.Pow(a, n) + Math.Pow(b, n) == Math.Pow(c, n);
+    }
+}
